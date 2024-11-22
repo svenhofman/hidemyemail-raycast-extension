3dfe7d7def5c75897b52a74b78fe2c703fdcc572
svenhofman <svenhofman25@gmail.com>
2024-11-22 11:56:34 +0100

    Initial commit

---
+++ src/components/forms/LoginForm.tsx
@@ -0,0 +1,39 @@
+import { ActionPanel, Action, Form } from "@raycast/api";
+import { useForm } from "@raycast/utils";
+
+interface Credentials {
+  appleID: string;
+  password: string;
+}
+
+export function LoginForm({ submit }: { submit: (credentials: Credentials) => void }) {
+  const { handleSubmit, itemProps } = useForm<Credentials>({
+    onSubmit(values) {
+      submit(values);
+    },
+    validation: {
+      appleID: (value) => {
+        if (!value?.trim()) {
+          return "Email cannot be empty";
+        }
+      },
+      password: (value) => {
+        if (!value?.trim()) {
+          return "Password cannot be empty";
+        }
+      },
+    },
+  });
+  return (
+    <Form
+      actions={
+        <ActionPanel>
+          <Action.SubmitForm title="Login" onSubmit={handleSubmit} />
+        </ActionPanel>
+      }
+    >
+      <Form.TextField title="Email" {...itemProps.appleID} />
+      <Form.PasswordField title="Password" {...itemProps.password} />
+    </Form>
+  );
+}
